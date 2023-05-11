8b1cc5384026085774009ada687ee5d9512be0d7
Sadaqat Ali <entrepreneurali@outlook.com>
2023-05-11 13:28:11 +1000

    feat:  added use-state-hook

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,19 +1,26 @@
-import React, { FormEvent } from 'react';
+import { FieldValues, useForm } from 'react-hook-form';
 
-//Add prevent deafulat property to prevent te default behaviour of the form
 const Form = () => {
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    console.log('Submitted');
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  //use this function to submit values to the server
+  const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
-        <label htmlFor="name" className="form-label">
-          Name
-        </label>
-        <input id="name" type="text" className="form-control" />
+        <label className="form-label">Name</label>
+        <input
+          {...(register('name'), { required: true, minLength: 3 })}
+          id="name"
+          type="text"
+          className="form-control"
+        />
+        {errors.name?.type === 'required' && <p>The name is rqeuired field</p>}
       </div>
       <button type="submit" className="btn btn-primary">
         Submit
